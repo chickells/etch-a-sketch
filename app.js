5fe9c105c6c123a768083e43dd285a3d94655b13
Chase <cnickellsumn@gmail.com>
2022-09-29 15:28:58 -0700

    feel like i'm getting closer but still pretty lost tbh
    struggling to target those grid auto styles
    then just need to change them via function and boom

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const grid = document.querySelector('.grid')
-console.log(grid);
+// const gridSize = prompt('How many boxes wide?')
+// let width = 960 / gridSize
+// console.log(width);
 
-const gridSize = prompt('How many boxes wide?')
+const test = document.querySelector('.grid');
+const styles = window.getComputedStyle(test)
+let columnTest = styles.getPropertyValue('grid-template-columns')
+console.log(columnTest);
 
-for (let i = 0; i < (gridSize*gridSize); i++) {
-    let newCell = document.createElement('div');
-    document.querySelector('.grid').appendChild(newCell);    
-    newCell.classList.add('cell');
-}
+columnTest = "repeat(4, 130px)"
+
+
+// for (let i = 0; i < (gridSize*gridSize); i++) {
+//     grid.style.setProperty('grid-template-columns', repeat(gridSize, (960/gridSize)px))
+//     let newCell = document.createElement('div');
+//     document.querySelector('.grid').appendChild(newCell);    
+//     newCell.classList.add('cell');
+// }
 
 // FUCK THIS SHIT MY BRAIN IS TOAST
 // https://www.w3schools.com/css/css_grid_item.asp
