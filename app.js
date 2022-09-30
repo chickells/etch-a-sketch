9bcf3cee8bc1c5205a649ac53a774463bc1c588c
Chase <cnickellsumn@gmail.com>
2022-09-30 07:21:00 -0700

    GOT MAKEGRID AND COLOR CLICK WORKING. Create click and hold then polish

--- app.js
+++ app.js
@@ -1,22 +1,53 @@
+// grid size
+
 const grid = document.querySelector('.grid')
-// const gridSize = prompt('How many boxes wide?')
-// let width = 960 / gridSize
-// console.log(width);
+let size = prompt('How big?')
 
-const test = document.querySelector('.grid');
-const styles = window.getComputedStyle(test)
-let columnTest = styles.getPropertyValue('grid-template-columns')
-console.log(columnTest);
+function setupGrid(size) {
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    // create cells
+    for (let i = 0; i < size * size; i++) {
+        const makeCell = document.createElement('div')
+        makeCell.classList.add('cell')
+        // MUST HAVE APPENDCHILD SO IT ACTUALLY ADDS THE DIV AND
+        // CHUCKS IT AT THE END OF THE LIST
+        // god fucking damn that was so simple lmao
+        grid.appendChild(makeCell)
+      }
+}
 
-columnTest = "repeat(4, 130px)"
+setupGrid(size)
+
+// ^^^^^^^^ THIS IS HOW IT WORKS YOU CUNT
+// god fucking dammit that shit was so frustrating
+// and i knew it'd be such a simple solution
+// instead of trying to select it, just make it EQUAL
+// what you want and it works
 
 
-// for (let i = 0; i < (gridSize*gridSize); i++) {
-//     grid.style.setProperty('grid-template-columns', repeat(gridSize, (960/gridSize)px))
-//     let newCell = document.createElement('div');
-//     document.querySelector('.grid').appendChild(newCell);    
-//     newCell.classList.add('cell');
-// }
+
+
+const cells = document.querySelectorAll('.cell')
+console.log(cells);
+
+// change color on click
+cells.forEach (function (cell) {
+    cell.addEventListener('click', function() {
+        this.style.backgroundColor = "red";
+    })
+})
+// HOLY FUCK IT WORKS
+// cells turns into node list (faux array)
+// take that array and run forEach on it
+// pass through (LITERALLY ANYTHING), and add click watcher
+// to that (anything), and do whatever function you want to it
+// LFG CUNTS
+
+
+// create rainbow function
+
+
 
 // FUCK THIS SHIT MY BRAIN IS TOAST
 // https://www.w3schools.com/css/css_grid_item.asp
